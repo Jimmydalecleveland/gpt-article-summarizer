7f915712146946c0dfb14ec76a3b0c3b2c0324ac
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2023-11-07 12:50:44 -0700

    add basic try catch for api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,13 +1,17 @@
 import { parse } from 'node-html-parser'
 
 export async function GET() {
-  const res = await fetch('https://blog.jimmydc.com/git-bisect/')
-  const text = await res.text()
+  try {
+    const res = await fetch('https://blog.jimmydc.com/git-bisect/')
+    const text = await res.text()
 
-  const root = parse(text)
-  const title = root.querySelector('title')?.text
-  const body = root.querySelector('body')?.text
+    const root = parse(text)
+    const title = root.querySelector('title')?.text
+    const body = root.querySelector('body')?.text
 
-  return Response.json({ title: title, body: body })
+    return Response.json({ title: title, body: body })
+  } catch (err) {
+    return Response.json({ error: err })
+  }
 }
 
